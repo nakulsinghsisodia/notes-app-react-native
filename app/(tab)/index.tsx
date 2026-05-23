4b2c60c791b3949d13f90bbcc07c2a80c6b59920
Nakul <nakulsinghsisodia07@gmail.com>
2026-05-23 17:20:18 +0530

    enhanced the ui

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,17 +1,16 @@
-import { data } from "@/constants/data";
+import { useNotes } from '@/context/NotesContext';
 import "@/global.css";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { router } from "expo-router";
 import { styled } from "nativewind";
 import { FlatList, Pressable, Text, View } from "react-native";
 import { SafeAreaView as RNsafeArea } from "react-native-safe-area-context";
-import { useNotes } from '@/context/NotesContext'
 
 const SafeAreaView = styled(RNsafeArea);
 
 export default function App() {
 
-  const {notes, deleteNote} = useNotes()
+  const { notes, deleteNote } = useNotes()
 
   return (
     <SafeAreaView className="flex-1 bg-gray-900">
@@ -30,26 +29,48 @@ export default function App() {
         }}
 
         ListHeaderComponent={
-          <View>
-            <Text className="text-3xl text-white mt-3 text-center">
-              Your Notes
-            </Text>
+          <View className="mb-6 mt-2">
 
-            {data.length ? (
-              <Text className="text-md text-white mb-5 mt-1 text-center">
-                Total notes: {notes.length}
-              </Text>
+            <View className="bg-gray-800 border border-gray-700 rounded-3xl px-5 py-6 flex-row items-center justify-between">
+
+              <View>
+                <Text className="text-3xl font-bold text-white tracking-wide">
+                  Your Notes
+                </Text>
+
+                <Text className="text-gray-400 mt-2 text-base">
+                  Capture your thoughts anytime
+                </Text>
+              </View>
+
+              <View className="bg-violet-600 p-4 rounded-2xl">
+                <Ionicons
+                  name="document-text"
+                  size={28}
+                  color="white"
+                />
+              </View>
+
+            </View>
+
+            {notes.length ? (
+              <View className="mt-4 self-start bg-gray-800 border border-gray-700 px-4 py-2 rounded-full">
+                <Text className="text-gray-300 font-medium">
+                  {notes.length} Notes Available
+                </Text>
+              </View>
             ) : null}
+
           </View>
         }
 
-        
+
         renderItem={({ item }) => {
 
           console.log(item)
-          
+
           return (
-            <View className="bg-gray-800 border border-gray-600 mb-4 w-full p-4 rounded-xl flex-row justify-between items-center">
+            <View className="bg-gray-800 border border-gray-700 mb-4 w-full p-4 rounded-xl flex-row justify-between items-center">
 
               <Pressable
                 className="flex-1"
