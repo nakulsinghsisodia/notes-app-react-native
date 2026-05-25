e82ef32d31badb89d0d1cee0bfd3ebf52622a214
Nakul <nakulsinghsisodia07@gmail.com>
2026-05-25 18:32:52 +0530

    icon and name changed

--- constants/data.ts
+++
@@ -1,6 +0,0 @@
-export const data:any = [
-  {id:1,title:'movies watch list',content: "hp, got,etc"},
-  {id:2,title:'sde',content: "hp, got,etc"},
-  {id:3,title:'movies',content: "hp, got,etc"},
-  {id:4,title:'watch list',content: "hp, got,etc"}
-]
