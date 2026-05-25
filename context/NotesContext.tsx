0ea22bf1bb686da0ce090335d00115a2f8a175f9
Nakul <nakulsinghsisodia07@gmail.com>
2026-05-25 18:04:31 +0530

    feat: created SQLite db

--- context/NotesContext.tsx
+++ context/NotesContext.tsx
@@ -1,4 +1,29 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useState, useEffect } from "react";
+import * as SQLite from 'expo-sqlite'
+
+type sqlDs = SQLite.SQLiteDatabase | null;
+
+let db: sqlDs = null
+
+async function openDB(): Promise<sqlDs> {
+    try{
+        db = await SQLite.openDatabaseAsync('notesDb')
+
+        await db.execAsync(`
+            PRAGMA journal_mode = WAL;
+            CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY, title TEXT NOT NULL, content TEXT NOT NULL);    
+        `)
+        console.log("db opened");
+        return db
+
+    }catch(e){
+        console.log("error while opening db",e);
+        return null
+    }
+}
+
+
+
 
 type Note = {
     id: number,
@@ -12,17 +37,17 @@ type NotesContextType = {
     addNote : (
         title: string,
         content: string
-    ) => void,
+    ) => Promise<void>,
 
     updateNote: (
         id: number,
         title: string,
         content: string
-    ) => void,
+    ) => Promise<void>,
 
     deleteNote : (
         id: number
-    ) => void
+    ) => Promise<void>
 }
 
 const NotesContext = createContext<NotesContextType | null>(null)
@@ -31,30 +56,64 @@ export const NotesProvider = ({children} : {children: ReactNode}) =>{
 
     const  [notes, setNotes] = useState<Note[]>([])
 
-    const addNote = (title:string, content: string) => {
-        const newNote = {
-            id: Date.now(),
-            title,
-            content
+    useEffect(()=>{
+        init()
+    },[])
+
+    async function init(){
+        await openDB()
+        await loadNotes()
+    }
+
+    async function loadNotes(){
+        try{
+            if(db==null)return 
+
+            const result = await db.getAllAsync<Note>('SELECT * FROM notes ORDER BY id DESC')
+
+            setNotes(result)
+        }catch(e){
+            console.log('error while loading data',e);
+            
         }
-
-        setNotes((prev)=>[...prev,newNote])
     }
 
-    const updateNote = (id: number, title: string, content: string) => {
-        setNotes((prev) => prev.map((note) =>
-            note.id === id ? {
-                ...note,
-                title,
-                content
-            } : note
-        ))
+    const  addNote = async(title:string, content: string) => {
+
+        try{
+            if (db==null) return
+            
+            const id:number = Date.now()
+
+            await db.runAsync('INSERT INTO notes(id,title,content) VALUES (?,?,?)',[id,title,content])
+            await loadNotes()
+
+        }catch(e){
+            console.log('error adding notes',e);    
+        }
     }
 
-    const deleteNote = (id:number) =>{
-        setNotes((prev)=> prev.filter(
-            (note) => note.id !== id 
-        ) )
+    const updateNote = async(id: number, title: string, content: string) => {
+        
+        try{
+            if(db==null)return
+
+            await db.runAsync('UPDATE notes SET title=?, content=? WHERE id=?',[title,content,id])
+            await loadNotes()
+        }catch(e){
+            console.log('error updating notes',e);    
+        }
+    }
+
+    const deleteNote = async(id:number) =>{
+        try{
+            if(db==null)return
+
+            await db.runAsync('DELETE FROM notes WHERE id=?',[id])
+            await loadNotes()
+        }catch(e){
+            console.log('error deleting notes',e);    
+        }
     }
 
     return (
